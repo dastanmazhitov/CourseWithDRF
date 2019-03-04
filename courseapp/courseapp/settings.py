0c0743bd0f6028a8ae464fcd2bbb407a56913d37
dastanmazhitov <38371570+dastanmazhitov@users.noreply.github.com>
2019-03-04 23:37:50 +0600

    Com

--- courseapp/courseapp/settings.py
+++ courseapp/courseapp/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'courseapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': config('DB_ENGINE'),
-        'NAME': config('DB_NAME'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'coursedb',
+        'USER': 'u_course',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -120,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
